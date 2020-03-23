7901016b38ed9ec5016e4062fc5af4c58c7cd136
AmiiirCom <amhoes1378@gmail.com>
2020-03-24 03:53:51 +0430

    HTML coding ended

--- index.php
+++ index.php
@@ -7,17 +7,78 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Hello, world!</title>
+    <title>about me</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
-
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-sm-12 col-md-4">
+            <div class="rounded px-2 py-4" id="box-bg">
+                <div class="text-center">
+                    <img src="assets/img/profile.jpg" class="rounded-circle profile-img" alt="...">
+                    <div class="clearfix mt-2">
+                        <h3 class="cursor-default">YOUR NAME</h3>
+                        <h6 class="cursor-default">YOUR JOB</h6>
+                    </div>
+                    <div class="clearfix mt-2 w-100">
+                        <div class="btn-group-vertical w-100">
+                            <button type="button" class="btn btn-primary">Facebook</button>
+                            <button type="button" class="btn btn-danger">Instagram</button>
+                            <button type="button" class="btn btn-info">Twitter</button>
+                            <button type="button" class="btn btn-dark">Github</button>
+                            <button type="button" class="btn btn-light">Telegram</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-8">
+            <div class="rounded py-2" id="box-bg">
+                <div class="w-100 text-center"><h6 class="cursor-default">yourname@root</h6></div>
+                <div class="clearfix text-left terminal mt-2 px-2 py-1">
+                    <p><small>Welcome to about me page, text</small></p>
+                    <p><span class="text-warning">—</span><small> for back to homepage click here.</small>
+                        <a class="btn btn-sm btn-outline-light" href="#"><small>back</small></a></p>
+                </div>
+            </div>
+            <br>
+            <div class="rounded py-2" id="box-bg">
+                <div class="w-100 text-center"><h6 class="cursor-default">skills</h6></div>
+                <div class="clearfix text-left terminal mt-2 px-2 py-1">
+                    <label for="skill-1">skill 1</label>
+                    <div class="progress" id="#skill-1">
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <label for="skill-2">skill 2</label>
+                    <div class="progress" id="#skill-2">
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <label for="skill-3">skill 3</label>
+                    <div class="progress" id="#skill-3">
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <label for="skill-4">skill 4</label>
+                    <div class="progress" id="#skill-4">
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center cursor-default text-light my-4" id="footer">
+        <p>made with <span class="text-danger">❤</span> by <a target="_blank" class="text-decoration-none text-light" href="https://iamiresmaeili7.ir/">Amir Esmaeili</a> - 2020</p>
+    </div>
+</div>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>
