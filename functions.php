0af69b5f5c59e8d860c3c081d02bb2b9dbf9535b
AmiiirCom <amhoes1378@gmail.com>
2020-03-25 19:06:25 +0430

    first version to run

--- functions.php
+++ functions.php
@@ -1,1 +1,202 @@
 <?php
+
+function page_title()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    print $data->page_title;
+}
+
+function favicon()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    print $data->favicon;
+}
+
+function name($strletter = "uppercase")
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    if ($strletter == "uppercase") {
+        $name = $data->name;
+        $name = strtoupper($name);
+        print $name;
+    } elseif ($strletter == "lowercase") {
+        $name = $data->name;
+        $name = strtolower($name);
+        print $name;
+    }
+}
+
+function job()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $job = $data->job;
+    $job = strtoupper($job);
+    print $job;
+}
+
+function profile_img()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    if ($data->profile_img == null) {
+        print "assets/img/profile.jpg";
+    }
+    print $data->profile_img;
+}
+
+function facebook()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $facebook = $data->facebook;
+    if ($facebook !== null) {
+        print "<a href='" . $facebook . "' target='_blank' class='btn btn-primary'>Facebook</a>";
+    }
+}
+
+function instagram()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $instagram = $data->instagram;
+    if ($instagram !== null) {
+        print "<a href='" . $instagram . "' target='_blank' class='btn btn-danger'>Instagram</a>";
+    }
+}
+
+function twitter()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $twitter = $data->twitter;
+    if ($twitter !== null) {
+        print "<a href='" . $twitter . "' target='_blank' class='btn btn-info'>Twitter</a>";
+    }
+}
+
+function github()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $github = $data->github;
+    if ($github !== null) {
+        print "<a href='" . $github . "' target='_blank' class='btn btn-dark'>Github</a>";
+    }
+}
+
+function telegram()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $telegram = $data->telegram;
+    if ($telegram !== null) {
+        print "<a href='" . $telegram . "' target='_blank' class='btn btn-light'>Telegram</a>";
+    }
+}
+
+function bio()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    print $data->bio;
+}
+
+function terminal_title()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $name = $data->name;
+    $name = strtolower(str_replace(" ", "", $name));
+    print $name;
+}
+
+function back_btn_url()
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $back_btn_url = $data->back_btn_url;
+    if ($back_btn_url == null) {
+        print "#";
+    }
+    print $back_btn_url;
+}
+
+function skill1($object)
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $name = $data->skill1->name;
+    $value = $data->skill1->value;
+    if ($object == "name") {
+        print $name;
+    } elseif ($object == "value") {
+        print $value;
+    } elseif ($name == null) {
+        print "";
+    } elseif ($value == null) {
+        print 0;
+    } else {
+        print "object not valid";
+    }
+}
+
+function skill2($object)
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $name = $data->skill2->name;
+    $value = $data->skill2->value;
+    if ($object == "name") {
+        print $name;
+    } elseif ($object == "value") {
+        print $value;
+    } elseif ($name == null) {
+        print "";
+    } elseif ($value == null) {
+        print 0;
+    } else {
+        print "object not valid";
+    }
+}
+
+function skill3($object)
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $name = $data->skill3->name;
+    $value = $data->skill3->value;
+    if ($object == "name") {
+        print $name;
+    } elseif ($object == "value") {
+        print $value;
+    } elseif ($name == null) {
+        print "";
+    } elseif ($value == null) {
+        print 0;
+    } else {
+        print "object not valid";
+    }
+}
+
+function skill4($object)
+{
+    $content = file_get_contents("content/content.json");
+    $data = json_decode($content);
+    $name = $data->skill4->name;
+    $value = $data->skill4->value;
+    if ($object == "name") {
+        print $name;
+    } elseif ($object == "value") {
+        print $value;
+    } elseif ($name == null) {
+        print "";
+    } elseif ($value == null) {
+        print 0;
+    } else {
+        print "object not valid";
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once "functions.php";
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,7 +13,10 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>about me</title>
+    <link rel="shortcut icon" href="<?php favicon(); ?>" type="image/x-icon">
+    <link rel="icon" href="<?php favicon(); ?>"
+    " type="image/x-icon">
+    <title><?php page_title(); ?></title>
 </head>
 <body>
 <div class="container mt-5">
@@ -18,18 +24,19 @@
         <div class="col-sm-12 col-md-4">
             <div class="rounded px-2 py-4" id="box-bg">
                 <div class="text-center">
-                    <img src="assets/img/profile.jpg" class="rounded-circle profile-img" alt="...">
+                    <img src="<?php profile_img(); ?>" class="rounded-circle profile-img"
+                         alt="<?php name("lowercase"); ?>">
                     <div class="clearfix mt-2">
-                        <h3 class="cursor-default">YOUR NAME</h3>
-                        <h6 class="cursor-default">YOUR JOB</h6>
+                        <h3 class="cursor-default"><?php print name(); ?></h3>
+                        <h6 class="cursor-default"><?php print job(); ?></h6>
                     </div>
                     <div class="clearfix mt-2 w-100">
                         <div class="btn-group-vertical w-100">
-                            <button type="button" class="btn btn-primary">Facebook</button>
-                            <button type="button" class="btn btn-danger">Instagram</button>
-                            <button type="button" class="btn btn-info">Twitter</button>
-                            <button type="button" class="btn btn-dark">Github</button>
-                            <button type="button" class="btn btn-light">Telegram</button>
+                            <?php facebook(); ?>
+                            <?php instagram(); ?>
+                            <?php twitter(); ?>
+                            <?php github(); ?>
+                            <?php telegram(); ?>
                         </div>
                     </div>
                 </div>
@@ -37,11 +44,12 @@
         </div>
         <div class="col-sm-12 col-md-8">
             <div class="rounded py-2" id="box-bg">
-                <div class="w-100 text-center"><h6 class="cursor-default">yourname@root</h6></div>
+                <div class="w-100 text-center"><h6 class="cursor-default"><?php terminal_title(); ?>@root</h6></div>
                 <div class="clearfix text-left terminal mt-2 px-2 py-1">
-                    <p><small>Welcome to about me page, text</small></p>
-                    <p><span class="text-success">—</span><small> for back to homepage click here.</small>
-                        <a class="btn btn-sm btn-outline-light" href="#"><small>back</small></a></p>
+                    <p><small><?php bio(); ?></small></p>
+                    <p><span class="text-success">—</span><small> for back to homepage click here</small>
+                        <a class="btn btn-sm btn-outline-light" href="<?php back_btn_url(); ?>"><small>back</small></a>
+                    </p>
                     <p class="cursor-default text-warning"><small>➜ ~ </small></p>
                 </div>
             </div>
@@ -49,28 +57,38 @@
             <div class="rounded py-2" id="box-bg">
                 <div class="w-100 text-center"><h6 class="cursor-default">skills</h6></div>
                 <div class="clearfix text-left skills mt-2 px-2 py-1">
-                    <label for="skill-1">skill 1</label>
+                    <label for="skill-1"><?php skill1("name"); ?></label>
                     <div class="progress" id="#skill-1">
-                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar"
+                             style="width: <?php skill1("value"); ?>%" aria-valuenow="<?php skill1("value"); ?>"
+                             aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <label for="skill-2">skill 2</label>
+                    <label for="skill-2"><?php skill2("name"); ?></label>
                     <div class="progress" id="#skill-2">
-                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar"
+                             style="width: <?php skill2("value"); ?>%" aria-valuenow="<?php skill2("value"); ?>"
+                             aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <label for="skill-3">skill 3</label>
+                    <label for="skill-3"><?php skill3("name"); ?></label>
                     <div class="progress" id="#skill-3">
-                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar"
+                             style="width: <?php skill3("value"); ?>%" aria-valuenow="<?php skill3("value"); ?>"
+                             aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <label for="skill-4">skill 4</label>
+                    <label for="skill-4"><?php skill4("name"); ?></label>
                     <div class="progress" id="#skill-4">
-                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar progress-bar-striped bg-info" role="progressbar"
+                             style="width: <?php skill4("value"); ?>%" aria-valuenow="<?php skill4("value"); ?>"
+                             aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="col-12 text-center cursor-default text-light my-4" id="footer">
-        <p>made with <span class="text-danger">❤</span> by <a target="_blank" class="text-decoration-none text-light" href="https://iamiresmaeili7.ir/">Amir Esmaeili</a> - 2020</p>
+        <p>made with <span class="text-danger">❤</span> by <a target="_blank" class="text-decoration-none text-light"
+                                                              href="https://iamiresmaeili7.ir/">Amir Esmaeili</a>
+            - <?php print date("Y"); ?></p>
     </div>
 </div>
 <!-- Optional JavaScript -->
